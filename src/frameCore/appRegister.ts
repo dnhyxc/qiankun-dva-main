fa0a2070b47f384d11e79108b4a4091009db1bc8
dengninghai <dengninghai@shinemo.com>
2021-11-29 20:02:59 +0800

    feat: bugfix of render failed Page

--- src/frameCore/appRegister.ts
+++ src/frameCore/appRegister.ts
@@ -5,10 +5,10 @@ import {
   addGlobalUncaughtErrorHandler,
   RegistrableApp,
   initGlobalState,
-} from 'qiankun';
-import { routes } from '@/constants';
-import * as appController from './controller/app';
-import render from './render/RenderApp';
+} from "qiankun";
+import { routes } from "@/constants";
+import * as appController from "./controller/app";
+import render from "./render/RenderApp";
 
 // 启动qiankun微前端应用方法
 const appStart = () => {
@@ -19,31 +19,31 @@ const appStart = () => {
   registerMicroApps(apps, {
     beforeLoad: [
       (app) => {
-        console.log('[LifeCycle] before load %c%s', 'color: green;', app);
+        console.log("[LifeCycle] before load %c%s", "color: green;", app);
         return Promise.resolve();
       },
     ],
     beforeMount: [
       (app) => {
-        console.log('[LifeCycle] before mount %c%s', 'color: green;', app);
+        console.log("[LifeCycle] before mount %c%s", "color: green;", app);
         return Promise.resolve();
       },
     ],
     afterMount: [
       (app) => {
-        console.log('[LifeCycle] after mount %c%s', 'color: green;', app);
+        console.log("[LifeCycle] after mount %c%s", "color: green;", app);
         return Promise.resolve();
       },
     ],
     beforeUnmount: [
       (app) => {
-        console.log('[LifeCycle] before unmount %c%s', 'color: green;', app);
+        console.log("[LifeCycle] before unmount %c%s", "color: green;", app);
         return Promise.resolve();
       },
     ],
     afterUnmount: [
       (app) => {
-        console.log('[LifeCycle] after unmount %c%s', 'color: green;', app);
+        console.log("[LifeCycle] after unmount %c%s", "color: green;", app);
         appController.afterUnmount();
         return Promise.resolve();
       },
@@ -55,22 +55,33 @@ const appStart = () => {
 
   // 微前端启动进入第一个子应用后回调函数
   runAfterFirstMounted(() => {
-    console.log('[MainApp] first app mounted');
+    console.log("[MainApp] first app mounted");
   });
 
   // 全局的未捕获异常处理器
   addGlobalUncaughtErrorHandler((event) => {
-    console.error(event);
-    const { message: msg } = event as any;
+    console.error("异常捕获", event);
+    const { message } = event as any;
+    const errorApp: any = [];
+    apps.forEach((i) => {
+      if (message && message.includes(i.name)) {
+        errorApp.splice(0, 1, i);
+      }
+    });
     // 加载失败时提示
-    if (msg && msg.includes('died in status LOADING_SOURCE_CODE')) {
+    if (
+      message &&
+      message.includes("died in status LOADING_SOURCE_CODE") &&
+      errorApp.length &&
+      window.location.pathname === errorApp[0].activeRule
+    ) {
       render(routes.failed);
     }
   });
 
   // 启动应用间通信机制;
   initGlobalState({
-    name: 'Qiankun-Dva',
+    name: "Qiankun-Dva",
   });
 };
 
